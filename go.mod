4888de9c241cf16772dbe479a6e115c9a47599db
phucvin <phucvin@gmail.com>
2022-11-24 09:49:04 +0000

    test wapc

---
+++ go.mod
@@ -0,0 +1,9 @@
+module waas
+
+go 1.19
+
+require (
+	github.com/Workiva/go-datastructures v1.0.53 // indirect
+	github.com/tetratelabs/wazero v1.0.0-pre.3 // indirect
+	github.com/wapc/wapc-go v0.5.5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/wapc/wapc-go"
+	"github.com/wapc/wapc-go/engines/wazero"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	engine := wazero.Engine()
+	ctx := context.Background()
+
+	guest, err := os.ReadFile("services/hello.wasm")
+	check(err)
+
+	module, err := engine.New(ctx, host, guest, &wapc.ModuleConfig{
+		Logger: wapc.PrintlnLogger,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	})
+	check(err)
+	defer module.Close(ctx)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		test(ctx, module)
+		wg.Done()
+	}()
+	go func() {
+		test(ctx, module)
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func test(ctx context.Context, module wapc.Module) {
+	instance, err := module.Instantiate(ctx)
+	check(err)
+	defer instance.Close(ctx)
+
+	result, err := instance.Invoke(ctx, "hello", []byte("john"))
+	check(err)
+
+	fmt.Println(string(result))
+}
+
+func host(ctx context.Context, binding, namespace, operation string, payload []byte) ([]byte, error) {
+	switch operation {
+	case "capitalize":
+		name := string(payload)
+		name = strings.Title(name)
+		return []byte(name), nil
+	}
+	return nil, fmt.Errorf("operation name not found: %s", operation)
+}
