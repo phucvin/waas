28c69d0c402c10638aa553df74624ffffd1426b6
phucvin <phucvin@gmail.com>
2022-11-24 22:15:36 +0000

    continue adding karmem, in hello service

--- services/hello/main.go
+++ services/hello/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 
 	wapc "github.com/wapc/wapc-guest-tinygo"
+	"karmem.org/golang"
+
+	"hello/waaskm"
 )
 
 var counter int32
@@ -22,12 +25,38 @@ func hello(payload []byte) ([]byte, error) {
 	counter += 1
 	fmt.Printf("hello called, counter = %d\n", counter)
 	_ = make([]byte, 100)
-	// Make a host call to capitalize the name.
-	nameBytes, err := wapc.HostCall("", "", "capitalize", payload)
+	nameBytes, err := invokeCapitalize(payload)
 	if err != nil {
 		return nil, err
 	}
 	// Format the message.
 	msg := "Hello, " + string(nameBytes)
 	return []byte(msg), nil
+}
+
+func invokeCapitalize(payload []byte) ([]byte, error) {
+	writer := karmem.NewWriter(1024)
+	inv := waaskm.Invocation{
+		Source: waaskm.Source{
+			Name: "hello",
+			Location: "global",
+		},
+		Destination: waaskm.Destination{
+			Name: "capitalize",
+			Location: "global",
+		},
+		Payload: payload,
+		Metadata: []waaskm.Metadata{},
+	}
+	_, err := inv.WriteAsRoot(writer);
+	if err != nil {
+		return nil, err
+	}
+	invBytes := writer.Bytes()
+
+	nameBytes, err := wapc.HostCall("", "", "invoke", invBytes)
+	if err != nil {
+		return nil, err
+	}
+	return nameBytes, nil
 }
