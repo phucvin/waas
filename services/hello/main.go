d8f2ab4dec1f6decdd861a8a835a7d961ebc9521
phucvin <phucvin@gmail.com>
2022-11-24 09:57:54 +0000

    add counter for hello

--- services/hello/main.go
+++ services/hello/main.go
@@ -6,7 +6,10 @@ import (
 	wapc "github.com/wapc/wapc-guest-tinygo"
 )
 
+var counter int32
+
 func main() {
+	counter = 0
 	// Register echo and fail functions
 	wapc.RegisterFunctions(wapc.Functions{
 		"hello": hello,
@@ -15,7 +18,8 @@ func main() {
 
 // hello will callback the host and return the payload
 func hello(payload []byte) ([]byte, error) {
-	fmt.Println("hello called")
+	counter += 1
+	fmt.Printf("hello called, counter = %d\n", counter)
 	_ = make([]byte, 100)
 	// Make a host call to capitalize the name.
 	nameBytes, err := wapc.HostCall("", "example", "capitalize", payload)
