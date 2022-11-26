02f67e81b4a4bf21f45b95133cc7e7e69ce3a0f4
phucvin <phucvin@gmail.com>
2022-11-26 08:51:14 +0000

    add ping service

---
+++ services/ping/main.ts
@@ -0,0 +1,21 @@
+import {
+  register,
+  handleCall,
+  hostCall,
+  handleAbort,
+  Result,
+} from "@wapc/as-guest";
+
+register("ping", function (payload: ArrayBuffer): Result<ArrayBuffer> {
+  return Result.ok(String.UTF8.encode("TODO: invoke pong"));
+});
+
+// This must be present in the entry file. Do not remove.
+
+export function __guest_call(operation_size: usize, payload_size: usize): bool {
+  return handleCall(operation_size, payload_size);
+}
+
+function abort(message: string | null, fileName: string | null, lineNumber: u32, columnNumber: u32): void {
+  handleAbort(message, fileName, lineNumber, columnNumber)
+}
